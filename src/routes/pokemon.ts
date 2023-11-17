45dac9e2db141ad972451278f567917d310185c4
TheNickest <53490469+TheNickest@users.noreply.github.com>
2023-11-17 18:16:08 +0100

    get by type implemented

--- src/routes/pokemon.ts
+++ src/routes/pokemon.ts
@@ -18,18 +18,18 @@ router.get("/:id", function (req: Request, res: Response, next: NextFunction): v
 
 /* GET Pokemon by English Name */
 router.get("/name/:name", function (req: Request, res: Response, next: NextFunction): void {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
   const name: string = String(req.params.name)
   const pokemon: JsonObject = pokedex.find((poke) => poke.name.english.toLowerCase() === name.toLowerCase()) as JsonObject
   res.json(pokemon)
-  return;
 });
 
 /* GET Pokemon by Type */
 router.get("/type/:type", function (req: Request, res: Response, next: NextFunction): void {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
+  let type: string = String(req.params.type)
+  const capitalize = (s: string) => s && s[0]?.toUpperCase() + s.slice(1)
+  type = capitalize(type)
+  const pokemon: JsonObject[] = pokedex.filter((poke) => poke.type.includes(type) === true) as JsonObject[]
+  res.json(pokemon)
 });
 
 /* GET Pokemon by HP */
