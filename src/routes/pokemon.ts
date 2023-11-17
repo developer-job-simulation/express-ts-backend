a900b5895f805f04703feeb0ad99ef2348416045
TheNickest <53490469+TheNickest@users.noreply.github.com>
2023-11-17 20:46:57 +0100

    compare HP implemented

--- src/routes/pokemon.ts
+++ src/routes/pokemon.ts
@@ -11,32 +11,54 @@ router.get("/", function (req: Request, res: Response, next: NextFunction): void
 
 /* GET Pokemon by Id. */
 router.get("/:id", function (req: Request, res: Response, next: NextFunction): void {
-  const id: number = Number(req.params.id)
-  const pokemon: JsonObject = pokedex.find((poke) => poke.id === id) as JsonObject
-  res.json(pokemon);
+  if (!req.query) {
+    const id: number = Number(req.params.id);
+    const pokemon: JsonObject = pokedex.find((poke) => poke.id === id) as JsonObject;
+    res.json(pokemon);
+  } else {
+    next();
+  }
 });
 
 /* GET Pokemon by English Name */
 router.get("/name/:name", function (req: Request, res: Response, next: NextFunction): void {
-  const name: string = String(req.params.name)
-  const pokemon: JsonObject = pokedex.find((poke) => poke.name.english.toLowerCase() === name.toLowerCase()) as JsonObject
-  res.json(pokemon)
+  const name: string = String(req.params.name);
+  const pokemon: JsonObject = pokedex.find(
+    (poke) => poke.name.english.toLowerCase() === name.toLowerCase()
+  ) as JsonObject;
+  res.json(pokemon);
 });
 
 /* GET Pokemon by Type */
 router.get("/type/:type", function (req: Request, res: Response, next: NextFunction): void {
-  let type: string = String(req.params.type)
-  const capitalize = (s: string) => s && s[0]?.toUpperCase() + s.slice(1)
-  type = capitalize(type)
-  const pokemon: JsonObject[] = pokedex.filter((poke) => poke.type.includes(type) === true) as JsonObject[]
-  res.json(pokemon)
+  let type: string = String(req.params.type);
+  const capitalize = (s: string) => s && s[0]?.toUpperCase() + s.slice(1);
+  type = capitalize(type);
+  const pokemon: JsonObject[] = pokedex.filter(
+    (poke) => poke.type.includes(type) === true
+  ) as JsonObject[];
+  res.json(pokemon);
 });
 
 /* GET Pokemon by HP */
 router.get("/hp", function (req: Request, res: Response, next: NextFunction): void {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
+  let pokemon: JsonObject[] = [];
+  if (Number(req.query.lt) < Number(req.query.gt)) {
+    res.status(404).json({ error: "Not found" });
+    return;
+  }
+  if (req.query.gt) {
+    pokemon = pokedex.filter((poke) => poke.base.HP > Number(req.query.gt)) as JsonObject[];
+  } else if (req.query.gte) {
+    pokemon = pokedex.filter((poke) => poke.base.HP >= Number(req.query.gte)) as JsonObject[];
+  } else if (req.query.lt) {
+    pokemon = pokedex.filter((poke) => poke.base.HP < Number(req.query.lt)) as JsonObject[];
+  } else if (req.query.lte) {
+    pokemon = pokedex.filter((poke) => poke.base.HP <= Number(req.query.lte)) as JsonObject[];
+  } else {
+    res.status(501).json({ error: "Invalid Operator. Must be one of [\"gt\",\"gte\",\"lt\",\"lte\"]" });
+  }
+  res.json(pokemon);
 });
 
 export default router;
