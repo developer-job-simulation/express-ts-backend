c65efa27234d5a1ba2368de92511d20865ac606d
skrooms <skrooms@protonmail.com>
2024-04-16 06:55:54 -0400

    Implemented route to GET pokemon by Id

--- src/routes/pokemon.ts
+++ src/routes/pokemon.ts
@@ -5,35 +5,42 @@ const router = Router();
 
 /* GET All Pokemon */
 router.get("/", function (req: Request, res: Response, next: NextFunction): void {
-  res.json(pokedex);
+	res.json(pokedex);
 });
 
 /* GET Pokemon by Id. */
-router.get("/:id", function (req: Request, res: Response, next: NextFunction): void {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
+router.get("/:id", function (req: Request, res: Response, next: NextFunction) {
+	// TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
+	const result = pokedex.find((pokemon) => {
+		return String(pokemon.id) === req.params.id;
+	});
+
+	if (!result) {
+		return res.status(404).json({ message: `No Pokemon found with id:${req.params.id}` });
+	}
+
+	return res.status(200).json(result);
 });
 
 /* GET Pokemon by English Name */
 router.get("/name/:name", function (req: Request, res: Response, next: NextFunction): void {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
+	// TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
+	res.status(501).json({ message: "Not Implemented" });
+	return;
 });
 
 /* GET Pokemon by Type */
 router.get("/type/:type", function (req: Request, res: Response, next: NextFunction): void {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
+	// TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
+	res.status(501).json({ message: "Not Implemented" });
+	return;
 });
 
 /* GET Pokemon by HP */
 router.get("/hp", function (req: Request, res: Response, next: NextFunction): void {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
+	// TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
+	res.status(501).json({ message: "Not Implemented" });
+	return;
 });
 
 export default router;
