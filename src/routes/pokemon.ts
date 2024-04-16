a429a990b3c4461ef687f23215aaad42b6781f0b
skrooms <skrooms@protonmail.com>
2024-04-16 07:13:19 -0400

    Implemented route to GET pokemon by English name

--- src/routes/pokemon.ts
+++ src/routes/pokemon.ts
@@ -4,13 +4,12 @@ import pokedex from "../db/pokedex.json";
 const router = Router();
 
 /* GET All Pokemon */
-router.get("/", function (req: Request, res: Response, next: NextFunction): void {
-	res.json(pokedex);
+router.get("/", function (req: Request, res: Response, next: NextFunction) {
+	return res.status(200).json(pokedex);
 });
 
 /* GET Pokemon by Id. */
 router.get("/:id", function (req: Request, res: Response, next: NextFunction) {
-	// TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
 	const result = pokedex.find((pokemon) => {
 		return String(pokemon.id) === req.params.id;
 	});
@@ -23,10 +22,17 @@ router.get("/:id", function (req: Request, res: Response, next: NextFunction) {
 });
 
 /* GET Pokemon by English Name */
-router.get("/name/:name", function (req: Request, res: Response, next: NextFunction): void {
+router.get("/name/:name", function (req: Request, res: Response, next: NextFunction) {
 	// TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-	res.status(501).json({ message: "Not Implemented" });
-	return;
+	const result = pokedex.find((pokemon) => {
+		return pokemon.name.english.toLowerCase() === req.params.name;
+	});
+
+	if (!result) {
+		return res.status(404).json({ message: `No Pokemon found with name: ${req.params.name}` });
+	}
+
+	return res.status(200).json(result);
 });
 
 /* GET Pokemon by Type */
